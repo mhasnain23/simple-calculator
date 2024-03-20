86a34531c7d8c89d16afde3e8f7f21d36f4d9c3a
mhasnain23 <tryfrelancer23@gmail.com>
2024-03-20 00:16:25 -0700

    nit

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+
+
+//conditional statement
+if (answer.operator === "Addition") {
+    console.log(chalk.green("Result: "), chalk.yellow(answer.firstNumber + answer.secondNumber));
+} else if (answer.operator === "Subtraction") {
+    console.log(chalk.green("Result: "), chalk.yellow(answer.firstNumber - answer.secondNumber));
+} else if (answer.operator === "Multiplication") {
+    console.log(chalk.green("Result: "), chalk.yellow(answer.firstNumber * answer.secondNumber));
+} else if (answer.operator === "Division") {
+    console.log(chalk.green("Result: "), chalk.yellow(answer.firstNumber / answer.secondNumber));
+} else {
+    console.log(chalk.red("Please select valid Operator"));
+}
