1c7fe4e85e09da098a51d059807a7b25e76f2701
mhasnain23 <tryfrelancer23@gmail.com>
2024-03-23 04:46:02 -0700

    nit

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 #! /usr/bin/env node
+
+
 import inquirer from "inquirer";
 import chalk from "chalk";
 
 const answer = await inquirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Enter first number",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Enter second number",
+        type: "number",
+        name: "secondNumber",
+    },
     {
         message: "select one of the operators to perform operations",
         type: "list",
@@ -13,16 +23,27 @@ const answer = await inquirer.prompt([
     },
 ]);
 
-
-//conditional statement
+// conditional statement
 if (answer.operator === "Addition") {
-    console.log(chalk.green("Result: "), chalk.yellow(answer.firstNumber + answer.secondNumber));
+    console.log(
+        chalk.green("Result: "),
+        chalk.yellow(answer.firstNumber + answer.secondNumber)
+    );
 } else if (answer.operator === "Subtraction") {
-    console.log(chalk.green("Result: "), chalk.yellow(answer.firstNumber - answer.secondNumber));
+    console.log(
+        chalk.green("Result: "),
+        chalk.yellow(answer.firstNumber - answer.secondNumber)
+    );
 } else if (answer.operator === "Multiplication") {
-    console.log(chalk.green("Result: "), chalk.yellow(answer.firstNumber * answer.secondNumber));
+    console.log(
+        chalk.green("Result: "),
+        chalk.yellow(answer.firstNumber * answer.secondNumber)
+    );
 } else if (answer.operator === "Division") {
-    console.log(chalk.green("Result: "), chalk.yellow(answer.firstNumber / answer.secondNumber));
+    console.log(
+        chalk.green("Result: "),
+        chalk.yellow(answer.firstNumber / answer.secondNumber)
+    );
 } else {
     console.log(chalk.red("Please select valid Operator"));
 }
